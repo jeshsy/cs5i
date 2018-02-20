d0e3fff68c8e5792bfa59e2e11fc13285ea3afe5
jeshsy <jeshsy@users.noreply.github.com>
2018-02-20 12:58:57 -0500

    basic passing props down to child

--- first/src/App.js
+++ first/src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Child from './Child';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          BLANKET CHILD
+          <Child blanket='500 thread count royalty blanket'/>
         </p>
       </div>
     );
