f1e36d8dd2048f8bdeca154e583feebc3c974a92
jeshsy <jeshsy@users.noreply.github.com>
2018-02-20 14:06:30 -0500

    cs5i 02-09 structure

--- first/src/App.js
+++ first/src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Child from './Child';
+import Second from './components/02';
+import Third from './components/03';
+import Fourth from './components/04';
+import Fifth from './components/05';
+import Sixth from './components/06';
+import Seventh from './components/07';
+import Eighth from './components/08';
+import Ninth from './components/09';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          BLANKET CHILD
-          <Child blanket='500 thread count royalty blanket'/>
-        </p>
+          BLANKET CHILDREN
+          <Second blanket='500 thread count royalty blanket'/>
+          <Third />
+          <Fourth />
+          <Fifth />
+          <Sixth />
+          <Seventh />
+          <Eighth />
+          <Ninth />
       </div>
     );
   }
---
+++ first/src/components/02/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class ComponentName extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    componentDidMount() {
+    }
+    render() {
+        return (
+            <div className='greeting'>
+            {}
+            {this.props.blanket}
+            </div>
+        );
+    }
+}
